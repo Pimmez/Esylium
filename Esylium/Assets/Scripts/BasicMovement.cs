7f4b17c1d2b75cafb3dcee6d243726ec1361e65e
pimhoreman@gmail.com <pimhoreman@gmail.com>
2020-02-18 22:05:40 +0100

    -Initialize Git -Added BasicMovemnt -Added Cinemachine for smooth camera movement

---
+++ Esylium/Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+
+	[SerializeField] private float movementSpeed;
+
+	private void Update()
+	{
+		Movement();
+	}
+        
+
+	private void Movement()
+	{
+		Vector3 _movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+
+
+		transform.position = transform.position + (_movement * movementSpeed) * Time.deltaTime;
+	}
+}
