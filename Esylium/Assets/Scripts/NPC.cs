9800dcf922baf32ae26270ea0146a6d070368cb8
pimhoreman@gmail.com <pimhoreman@gmail.com>
2020-02-28 15:25:28 +0100

    -Added Some part of dialogue system -Added NPC.cs -Added Dialogue.cs -Added DialogueManager.cs -Added DialogueTrigger.cs -Changed GameScene

---
+++ Esylium/Assets/Scripts/NPC.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class NPC : MonoBehaviour
+{
+	public static Action ActivateDialogueEvent;
+
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		if (collision.gameObject.tag == Tags.PLAYER)
+		{
+			if (Input.GetKeyDown(KeyCode.E))
+			{
+				if (ActivateDialogueEvent != null)
+				{
+					ActivateDialogueEvent();
+				}
+			}
+		}
+	}
+}
