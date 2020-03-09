c56eb0811b4359bb33c887732b3d323266825db6
pimhoreman@gmail.com <pimhoreman@gmail.com>
2020-03-09 13:25:29 +0100

    Added a suspicion slider for gameplay
    -Added SuspicionSystem.cs
    -Changed DialogueManager.cs
    -Changed GameScene.unity
    -Added new narrativeink file

---
+++ Esylium/Assets/Scripts/SuspicionSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class SuspicionSystem : MonoBehaviour
+{
+	[SerializeField] private int sliderValue;
+	[SerializeField] private Slider slider;
+
+	private void RaiseSuspicionSlider(string _suspicionTag)
+	{
+		slider.value = sliderValue;
+	}
+
+	private void OnEnable()
+	{
+		DialogueManager.SuspicionTagEvent += RaiseSuspicionSlider;		
+	}
+
+	private void OnDisable()
+	{
+		DialogueManager.SuspicionTagEvent -= RaiseSuspicionSlider;
+	}
+}
