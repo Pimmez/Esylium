f29d8d37c599740f4ad61a1247c8d0590133950a
pimhoreman@gmail.com <pimhoreman@gmail.com>
2020-03-09 14:50:31 +0100

    Added a lerp for the smoothness of the slider
    -Added SuspicionSystem.cs

--- Esylium/Assets/Scripts/SuspicionSystem.cs
+++ Esylium/Assets/Scripts/SuspicionSystem.cs
@@ -6,12 +6,33 @@ using UnityEngine.UI;
 
 public class SuspicionSystem : MonoBehaviour
 {
+	public int SliderValue { get { return sliderValue; } set { sliderValue = value; } }
 	[SerializeField] private int sliderValue;
+
 	[SerializeField] private Slider slider;
+	[SerializeField] private float delta = 1f;
+
+	private bool canChange = false;
+
+	private void Update()
+	{			
+		if (canChange)
+		{
+			slider.value = Mathf.Lerp(slider.value, sliderValue, delta * Time.deltaTime);
+			//slider.value = Mathf.MoveTowards(slider.value, sliderValue, delta * Time.deltaTime);
+
+			//Slidervalue will count in miliseconds on and on without reaching his destination. After a certain point we will lock it to an even number.
+			if(slider.value >= (sliderValue - 0.001))
+			{
+				slider.value = sliderValue;
+				canChange = false;
+			}
+		}
+	}
 
 	private void RaiseSuspicionSlider(string _suspicionTag)
 	{
-		slider.value = sliderValue;
+		canChange = true;
 	}
 
 	private void OnEnable()
