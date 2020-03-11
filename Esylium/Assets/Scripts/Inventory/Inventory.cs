0e024ae7890668ed03edb54dd0f27c2c436c4276
pimhoreman@gmail.com <pimhoreman@gmail.com>
2020-03-11 13:50:10 +0100

    Added Inventory system not yet tested.
    -Added Inventory.cs
    -Added InventoryUI.cs
    -Added InventorySlot.cs
    -Added Item.cs
    -Added Sprites for the inventory
    -Added ScriptableObjects Item with asset folder
    -Added New Prefabs for the Inventory UI
    -Added ItemPickup.cs

---
+++ Esylium/Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+	#region Singleton
+
+	public static Inventory instance;
+
+	private void Awake()
+	{
+		if (instance != null)
+		{
+			Debug.LogWarning("More then one instance of inventory is active in the scene");
+			return;
+		}
+		instance = this;
+	}
+	#endregion
+
+	public delegate void OnItemChanged();
+	public OnItemChanged onItemChangedCallback;
+	public List<Item> items = new List<Item>();
+
+	public int space = 8;
+
+	public bool Add(Item item)
+	{
+		if (items.Count >= space)
+		{
+			Debug.Log("Not enough room");
+			return false;
+		}
+		items.Add(item);
+
+		if (onItemChangedCallback != null)
+		{ 
+			onItemChangedCallback.Invoke();
+		}
+		return true;
+	}
+
+	public void Remove(Item item)
+	{
+		items.Remove(item);
+		if (onItemChangedCallback != null)
+		{
+			onItemChangedCallback.Invoke();
+		}
+	}
+}
---
+++ Esylium/Assets/Scripts/Inventory/ItemPickup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+	public Item item;
+	[SerializeField] private float interactionRange = 2f;
+
+	private void Update()
+	{
+		CheckNearObjects();
+	}
+
+	private void CheckNearObjects()
+	{
+		Collider[] objects = Physics.OverlapSphere(transform.position, interactionRange);
+
+		foreach (Collider col in objects)
+		{
+			if (col.tag == Tags.PLAYER)
+			{
+				PickUp();
+			}
+		}
+	}
+
+	private void PickUp()
+	{
+		Debug.Log("Picked Up " + item.title);
+		Inventory.instance.Add(item);
+		Destroy(gameObject);
+	}
+}
