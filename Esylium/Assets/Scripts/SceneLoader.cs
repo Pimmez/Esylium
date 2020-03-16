efee9b6e73c89cd5f46ab6a40fc0d2919e0f6792
pimhoreman@gmail.com <pimhoreman@gmail.com>
2020-03-16 12:17:10 +0100

    -Added HitCollider.cs for trigger collisions -Added SceneLoader.cs with singleton for sceneswitching -Changed Tags.cs with another tag

---
+++ Esylium/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+	#region Singleton
+	public static SceneLoader instance;
+
+	private void Awake()
+	{
+		if (instance != null)
+		{
+			Debug.LogWarning("More then one instance of inventory is active in the scene");
+			return;
+		}
+		instance = this;
+
+		GameObject[] objs = GameObject.FindGameObjectsWithTag(Tags.GAMECONTROLLER);
+
+		if (objs.Length > 1)
+		{
+			Destroy(this.gameObject);
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+	#endregion
+
+	public void StartGame(int _sceneIndex)
+	{
+		SceneManager.LoadScene(_sceneIndex);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+	public void SwitchToScene(int _sceneIndex)
+	{
+		SceneManager.LoadSceneAsync(_sceneIndex);
+	}
+}
